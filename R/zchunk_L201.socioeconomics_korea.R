bf706c4c503d8c283140be9b553156ae8ed1047d
rohmin9122 <rohmin9122@gmail.com>
2019-10-22 16:29:54 +0900

    US GDP

--- R/zchunk_L201.socioeconomics_korea.R
+++ R/zchunk_L201.socioeconomics_korea.R
@@ -9,6 +9,7 @@
 #' the generated outputs: \code{L201.InterestRate_GCAM_korea}, \code{L201.Pop_GCAM_korea}, \code{L201.BaseGDP_GCAM_korea}, \code{L201.LaborForceFillout_GCAM_korea}, \code{L201.LaborProductivity_GCAM_korea}. The corresponding file in the
 #' original data system was \code{L201.socioeconomics_USA.R} (gcam-usa level2).
 #' @details Interest rate, population, and GDP for gcam-korea.
+#' Note that Ulsan was promoted to metropolitan city in 1997. so, we use very samll value instead of base year value to prevent model errors.
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr filter mutate select
 #' @importFrom tidyr gather spread
@@ -16,9 +17,10 @@
 module_gcam.korea_L201.socioeconomics <- function(command, ...) {
   if(command == driver.DECLARE_INPUTS) {
     return(c(FILE = "gcam-korea/states_subregions",
+             FILE = "gcam-korea/GDP_Growth",
              "L100.Pop_thous_korea",
              "L100.GDP_mil90usd_korea",
-             "L100.pcGDP_thous90usd_korea",
+             #"L100.pcGDP_thous90usd_korea",
              "L102.pcgdp_thous90USD_GCAM3_ctry_Y"
              ))
   } else if(command == driver.DECLARE_OUTPUTS) {
@@ -36,31 +38,35 @@ module_gcam.korea_L201.socioeconomics <- function(command, ...) {
 
     # Load required inputs
     states_subregions <- get_data(all_data, "gcam-korea/states_subregions")
-    L100.Pop_thous_state <- get_data(all_data, "L100.Pop_thous_korea")
-    L100.GDP_mil90usd_state <- get_data(all_data, "L100.GDP_mil90usd_korea")
-    L100.pcGDP_thous90usd_korea <- get_data(all_data, "L100.pcGDP_thous90usd_korea")
+    L100.Pop_thous_korea <- get_data(all_data, "L100.Pop_thous_korea")
+    L100.GDP_mil90usd_korea <- get_data(all_data, "L100.GDP_mil90usd_korea")
+    # L100.pcGDP_thous90usd_korea <- get_data(all_data, "L100.pcGDP_thous90usd_korea")
     L102.pcgdp_thous90USD_GCAM3_ctry_Y<- get_data(all_data, "L102.pcgdp_thous90USD_GCAM3_ctry_Y")
+    GDP_Growth <- get_data(all_data, "gcam-korea/GDP_Growth")
     # ===================================================
     # NOTE: Socioeconomics for grid regions are dealt with in module_gcam.usa_L223.electricity_USA
 
     # L201.InterestRate: Interest rates by region
     L201.InterestRate <- tibble(region = states_subregions$state, interest.rate = socioeconomics.DEFAULT_INTEREST_RATE)
 
-    # L201.Pop_GCAMUSA: Population by region from the GCAM 3.0 core scenario
-    L201.Pop_GCAM_korea <- L100.Pop_thous_state %>%
+    L201.Pop_GCAM_korea <- L100.Pop_thous_korea %>%
       filter(year %in% MODEL_YEARS) %>%
       rename(totalPop = value,
              region = state) %>%
       mutate(totalPop = round(totalPop, socioeconomics.POP_DIGITS))
 
-    # L201.BaseGDP_GCAMUSA: Base GDP for GCAM-USA scenario
-    L201.BaseGDP_GCAM_korea <- L100.GDP_mil90usd_state %>%
+    L201.Pop_GCAM_korea[which(L201.Pop_GCAM_korea$region=="US" &
+                                L201.Pop_GCAM_korea$year %in% c(1975,1990)),]$totalPop <- L201.Pop_GCAM_korea[which(L201.Pop_GCAM_korea$region=="US" &
+                                                                                                                            L201.Pop_GCAM_korea$year==2005),]$totalPop
+
+    L201.BaseGDP_GCAM_korea <- L100.GDP_mil90usd_korea %>%
       filter(year == min(MODEL_YEARS)) %>%
       rename(baseGDP = value, region = state) %>%
+      # To prevent model error, we use gdp1998 instead of gdp1975
+      mutate(baseGDP=if_else(region=="US",
+                             L100.GDP_mil90usd_korea[which(L100.GDP_mil90usd_korea$year==1998 &L100.GDP_mil90usd_korea$state=="US"),]$value,
+                             baseGDP)) %>%
       mutate(baseGDP = round(baseGDP, socioeconomics.GDP_DIGITS)) %>%
-      # Ulsan was promoted to metropolitan city in 1997. So, the base gdp (1975) is zero
-      # 0.000001 is a very small number, that will prevent model error
-      mutate(baseGDP=if_else(baseGDP==0, 0.000001, baseGDP)) %>%
       select(-year)
 
     # L201.LaborForceFillout: Labor force participation and productivity for all scenarios
@@ -70,37 +76,43 @@ module_gcam.korea_L201.socioeconomics <- function(command, ...) {
                                      laborforce = socioeconomics.DEFAULT_LABORFORCE)
 
 
-    popShare_korea <- L100.Pop_thous_state %>%
+
+    L100.GDP_mil90usd_korea <- arrange(L100.GDP_mil90usd_korea, state, year)
+    L100.GDP_mil90usd_korea %>%
       filter(year %in% MODEL_YEARS) %>%
-      group_by(year) %>%
-      mutate(popShare=value/sum(value)) %>%
-      select(state, year, popShare, -value) %>%
-      ungroup
+      mutate(prevValue = lag(value)) %>%
+      mutate(growth.rate = value/prevValue) %>%
+      ungroup() %>%
+      select(state, year, growth.rate)-> hitorical_gdp_growth
 
-    pcGDP_korea <- popShare_korea %>%
-      left_join_error_no_match(L102.pcgdp_thous90USD_GCAM3_ctry_Y %>%
-                                 filter(tolower(iso) == "kor", year %in% MODEL_YEARS), by = "year") %>%
-      mutate(pcGDP=popShare*value) %>%
-      select(state, year, pcGDP)
+    model_years_gdp_growth <- tidyr::crossing(region = gcamkorea.STATES, year = MODEL_YEARS)
+    model_years_gdp_growth %>%
+      left_join(hitorical_gdp_growth, by=c("region"="state", "year")) %>%
+      left_join(GDP_Growth, by="year") %>%
+      mutate(gdp.growth=if_else(year<=CALIBRATION_YEAR, growth.rate, value),
+             prevYear=lag(year)) %>%
+      mutate(timestep=year-prevYear) %>%
+      select(region, year, gdp.growth, timestep)-> model_years_gdp_growth
 
 
-    # LABOR PRODUCTIVITY GROWTH RATE CALCULATION
-    # Labor productivity growth is calculated from the change in per-capita GDP ratio in each time period
-    # Calculate the growth rate in per-capita GDP
-    L201.LaborProductivity_GCAM_korea <- pcGDP_korea %>%
-      # In order to calculate growth rate we need to know how much GDP grew and number of years between periods
-      mutate(growth = pcGDP / lag(pcGDP),
-             timestep = year - lag(year),
-             laborproductivity = round(growth ^ (1 / timestep) - 1, socioeconomics.LABOR_PRODUCTIVITY_DIGITS)) %>%
+    L100.Pop_thous_korea <- arrange(L100.Pop_thous_korea, state, year)
+    L100.Pop_thous_korea %>%
+      filter(year %in% MODEL_YEARS) %>%
+      mutate(prevValue = lag(value)) %>%
+      mutate(pop.growth = value/prevValue) %>%
+      ungroup() %>%
+      select(state, year, pop.growth)-> pop_growth
+
+    model_years_gdp_growth %>%
+      left_join(pop_growth, by=c("region"="state", "year")) %>%
       # Remove the first model year, since it has no previous period to calculate growth rate
       filter(year != min(MODEL_YEARS)) %>%
-      replace_na(list(laborproductivity = 0)) %>%
-      ungroup %>%
-      # Renaming to region because region column needed for write_to_all_states
-      rename(region=state)%>%
-      select(region, year, laborproductivity)
+      mutate(laborproductivity = round((gdp.growth/pop.growth) ^ (1 / timestep) - 1, socioeconomics.LABOR_PRODUCTIVITY_DIGITS)) -> L201.LaborProductivity_GCAM_korea
 
-    #L201.LaborProductivity_GCAM_korea[is.infinite(L201.LaborProductivity_GCAM_korea$laborproductivity),"laborproductivity"] <- 0
+    # Usan!!!!!
+    L201.LaborProductivity_GCAM_korea[which(L201.LaborProductivity_GCAM_korea$region=="US" &
+                                             L201.LaborProductivity_GCAM_korea$year%in%c(1990,2005)),]$laborproductivity <- 0
+
 
     # ===================================================
 
@@ -143,7 +155,7 @@ module_gcam.korea_L201.socioeconomics <- function(command, ...) {
       add_comments("Values from L100.pcGDP_thous90usd_korea used to calculate annual growth") %>%
       add_comments("Korea value written to all states") %>%
       add_legacy_name("L201.LaborProductivity_GCAM_korea") %>%
-      add_precursors("L100.pcGDP_thous90usd_korea",
+      add_precursors("gcam-korea/GDP_Growth",
                      "L102.pcgdp_thous90USD_GCAM3_ctry_Y",
                      "L100.Pop_thous_korea") ->
       L201.LaborProductivity_GCAM_korea
